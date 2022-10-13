7c1e337c007b9cb419308025144f11a8ef77e3b4
Evgeniy Naumenko <naumenko_evgeniy03@mail.ru>
2022-10-13 16:20:59 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+
+	intToRoman(110)
+
+}
+
+func intToRoman(num int) string {
+	nums := []int{1100, 1000, 900, 600, 500, 400, 110, 100, 90, 60, 50, 40, 10, 9, 5, 4, 1}
+	symbols := []string{"MC", "M", "CM", "DC", "D", "CD", "CX", "C", "XC", "LX", "L", "XL", "X", "IX", "V", "IV", "I"}
+	res, i := "", 0
+	for num != 0 {
+		for num < nums[i] {
+			i++
+
+		}
+		num -= nums[i]
+		res += symbols[i]
+
+		print("-")
+		print(res)
+
+	}
+	return res
+}
